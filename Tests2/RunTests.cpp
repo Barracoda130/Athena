3cc76439e4d80139d1003813f5eb72654054a177
Barracuda130 <lcompton-burnett@hotmail.com>
2025-12-22 11:54:05 +0000

    Other integer tests mostly setup

--- Tests2/RunTests.cpp
+++ Tests2/RunTests.cpp
@@ -1,6 +1,7 @@
 #include "CppUnitTest.h"
 
 #include <iostream>
+#include <functional>
 
 #include "TestDataFile.hpp"
 #include "MpfrInclude.hpp"
@@ -64,135 +65,191 @@ static std::string mpfr_tToStr( mpfr_t a_Num )
 	return result;
 }
 
+void runTest( TestData::FileReader& a_File, 
+			  std::function<void(mpfr_t, mpfr_t, mpfr_t, mpfr_rnd_t)> a_F1,
+			  std::function<long long( long long, long long )> a_F2 )
+{
+	const char* n1 = a_File.getNextA();
+	const char* n2 = a_File.getNextB();
+	while ( !a_File.eofReached() )
+	{
+		// Run through MPFR
+		mpfr_t mpfrResult;
+
+		mpfr_t num1, num2;
+		mpfr_init2( num1, 512 );
+		mpfr_init2( num2, 512 );
+		mpfr_init2( mpfrResult, 512 );
+
+		mpfr_set_str( num1, n1, 10, MPFR_RNDD );
+		mpfr_set_str( num2, n2, 10, MPFR_RNDD );
+
+		a_F1( mpfrResult, num1, num2, MPFR_RNDD );
+
+		std::string mpfrResultString = mpfr_tToStr( mpfrResult );
+
+		long long n1Int = std::stoll( n1 );
+		long long n2Int = std::stoll( n2 );
+
+		std::string myResult = std::to_string( a_F2(n1Int, n2Int) );
+
+		Assert::AreEqual( mpfrResultString, myResult );
+
+		n1 = a_File.getNextA();
+		n2 = a_File.getNextB();
+	}
+}
+
 namespace Integers
 {
+	// Runs once before all tests to ensure that the file has been created
+	TEST_MODULE_INITIALIZE( createTestData )
+	{
+		// Create a file reader, the read types don't matter
+		TestData::FileReader file( false, TestData::POSITIVE, false, TestData::POSITIVE );
+
+		// If eof is reached then the FileReader class has already closed the file
+		// If the test data doesn't yet exist, generate it
+		if ( file.eofReached() )
+			TestData::generateData( 10 );
+	}
+
 	TEST_CLASS( Addition )
 	{
-		void simpleMpfrAdd( const char* a_Num1, const char* a_Num2, mpfr_t& a_Result )
+		static long long basicAdd( long long a_N1, long long a_N2 )
 		{
-			mpfr_t num1, num2;
-			mpfr_init2( num1, 512 );
-			mpfr_init2( num2, 512 );
-			mpfr_init2( a_Result, 512 );
-
-			mpfr_set_str( num1, a_Num1, 10, MPFR_RNDD );
-			mpfr_set_str( num2, a_Num2, 10, MPFR_RNDD );
-
-			mpfr_add( a_Result, num1, num2, MPFR_RNDD );
-		}
-
-		// Runs once before all tests to ensure that the file has been created
-		TEST_CLASS_INITIALIZE( createTestData )
-		{
-			// Create a file reader, the read types don't matter
-			TestData::FileReader file( false, TestData::POSITIVE, false, TestData::POSITIVE );
-
-			// If eof is reached then the FileReader class has already closed the file
-			// If the test data doesn't yet exist, generate it
-			if ( file.eofReached() )
-				TestData::generateData( 10 );
+			return a_N1 + a_N2;
 		}
 		TEST_METHOD( positive )
 		{
 			TestData::FileReader file( false, TestData::POSITIVE, false, TestData::POSITIVE );
 
-			const char* n1 = file.getNextA();
-			const char* n2 = file.getNextB();
-			while ( !file.eofReached() )
-			{
-				// Run through MPFR
-				mpfr_t mpfrResult;
-
-				simpleMpfrAdd( n1, n2, mpfrResult );
-				std::string mpfrResultString = mpfr_tToStr( mpfrResult );
-
-				long long n1Int = std::stoll( n1 );
-				long long n2Int = std::stoll( n2 );
-
-				std::string myResult = std::to_string( n1Int + n2Int );
-
-				Assert::AreEqual( mpfrResultString, myResult );
-
-				n1 = file.getNextA();
-				n2 = file.getNextB();
-			}
+			runTest( file, mpfr_add, basicAdd );
 		}
 
 		TEST_METHOD( negative )
 		{
 			TestData::FileReader file( false, TestData::NEGATIVE, false, TestData::NEGATIVE );
 
-			const char* n1 = file.getNextA();
-			const char* n2 = file.getNextB();
-			while ( !file.eofReached() )
-			{
-				// Run through MPFR
-				mpfr_t mpfrResult;
-
-				simpleMpfrAdd( n1, n2, mpfrResult );
-				std::string mpfrResultString = mpfr_tToStr( mpfrResult );
-
-				long long n1Int = std::stoll( n1 );
-				long long n2Int = std::stoll( n2 );
-
-				std::string myResult = std::to_string( n1Int + n2Int );
-
-				Assert::AreEqual( mpfrResultString, myResult );
-
-				n1 = file.getNextA();
-				n2 = file.getNextB();
-			}
+			runTest( file, mpfr_add, basicAdd );
 		}
 
 		TEST_METHOD( positiveAndNegative )
 		{
 			TestData::FileReader file( false, TestData::POSITIVE, false, TestData::NEGATIVE );
 
-			const char* n1 = file.getNextA();
-			const char* n2 = file.getNextB();
-			while ( !file.eofReached() )
-			{
-				// Run through MPFR
-				mpfr_t mpfrResult;
-
-				simpleMpfrAdd( n1, n2, mpfrResult );
-				std::string mpfrResultString = mpfr_tToStr( mpfrResult );
-
-				long long n1Int = std::stoll( n1 );
-				long long n2Int = std::stoll( n2 );
-
-				std::string myResult = std::to_string( n1Int + n2Int );
-
-				Assert::AreEqual( mpfrResultString, myResult );
-
-				n1 = file.getNextA();
-				n2 = file.getNextB();
-			}
+			runTest( file, mpfr_add, basicAdd );
 
 			// Now run it with negative + positive rather than positive + negative
 			// to ensure both ways round work
 			file.reset( false, TestData::NEGATIVE, false, TestData::POSITIVE );
 
-			n1 = file.getNextA();
-			n2 = file.getNextB();
-			while ( !file.eofReached() )
-			{
-				// Run through MPFR
-				mpfr_t mpfrResult;
+			runTest( file, mpfr_add, basicAdd );
+		}
+	};
 
-				simpleMpfrAdd( n1, n2, mpfrResult );
-				std::string mpfrResultString = mpfr_tToStr( mpfrResult );
+	TEST_CLASS( Subtraction )
+	{
+		static long long basicSub( long long a_N1, long long a_N2 )
+		{
+			return a_N1 - a_N2;
+		}
 
-				long long n1Int = std::stoll( n1 );
-				long long n2Int = std::stoll( n2 );
+		TEST_METHOD( positive )
+		{
+			TestData::FileReader file( false, TestData::POSITIVE, false, TestData::POSITIVE );
 
-				std::string myResult = std::to_string( n1Int + n2Int );
+			runTest( file, mpfr_sub, basicSub );
+		}
 
-				Assert::AreEqual( mpfrResultString, myResult );
+		TEST_METHOD( negative )
+		{
+			TestData::FileReader file( false, TestData::NEGATIVE, false, TestData::NEGATIVE );
 
-				n1 = file.getNextA();
-				n2 = file.getNextB();
-			}
+			runTest( file, mpfr_sub, basicSub );
+		}
+
+		TEST_METHOD( positiveAndNegative )
+		{
+			TestData::FileReader file( false, TestData::POSITIVE, false, TestData::NEGATIVE );
+
+			runTest( file, mpfr_sub, basicSub );
+
+			// Now run it with negative + positive rather than positive + negative
+			// to ensure both ways round work
+			file.reset( false, TestData::NEGATIVE, false, TestData::POSITIVE );
+
+			runTest( file, mpfr_sub, basicSub );
+		}
+	};
+
+	TEST_CLASS( Multiplication )
+	{
+		static long long basicMult( long long a_N1, long long a_N2 )
+		{
+			return a_N1 * a_N2;
+		}
+
+		TEST_METHOD( positive )
+		{
+			TestData::FileReader file( false, TestData::POSITIVE, false, TestData::POSITIVE );
+
+			runTest( file, mpfr_mul, basicMult );
+		}
+
+		TEST_METHOD( negative )
+		{
+			TestData::FileReader file( false, TestData::NEGATIVE, false, TestData::NEGATIVE );
+
+			runTest( file, mpfr_mul, basicMult );
+		}
+
+		TEST_METHOD( positiveAndNegative )
+		{
+			TestData::FileReader file( false, TestData::POSITIVE, false, TestData::NEGATIVE );
+
+			runTest( file, mpfr_mul, basicMult );
+
+			// Now run it with negative + positive rather than positive + negative
+			// to ensure both ways round work
+			file.reset( false, TestData::NEGATIVE, false, TestData::POSITIVE );
+
+			runTest( file, mpfr_mul, basicMult );
+		}
+	};
+
+	TEST_CLASS( Division )
+	{
+		static long long basicSub( long long a_N1, long long a_N2 )
+		{
+			return a_N1 - a_N2;
+		}
+
+		TEST_METHOD( positive )
+		{
+			TestData::FileReader file( false, TestData::POSITIVE, false, TestData::POSITIVE );
+
+			runTest( file, mpfr_div, basicSub );
+		}
+
+		TEST_METHOD( negative )
+		{
+			TestData::FileReader file( false, TestData::NEGATIVE, false, TestData::NEGATIVE );
+
+			runTest( file, mpfr_div, basicSub );
+		}
+
+		TEST_METHOD( positiveAndNegative )
+		{
+			TestData::FileReader file( false, TestData::POSITIVE, false, TestData::NEGATIVE );
+
+			runTest( file, mpfr_div, basicSub );
+
+			// Now run it with negative + positive rather than positive + negative
+			// to ensure both ways round work
+			file.reset( false, TestData::NEGATIVE, false, TestData::POSITIVE );
+
+			runTest( file, mpfr_div, basicSub );
 		}
 	};
 
