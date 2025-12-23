108a02fabc7fa3e5b9c70c1cf0e849b72ad8801f
Barracuda130 <lcompton-burnett@hotmail.com>
2025-12-23 17:31:06 +0000

    I think this is the basic framework i will use. Could change though and implementations have not been done.

---
+++ Athena/src/Number.cpp
@@ -0,0 +1,129 @@
+#include "Number.hpp"
+#include <cassert>
+
+namespace Athena
+{
+	Number::Number()
+	{
+		m_Precision = 0;
+		m_Exp = 0;
+		m_Sign = NOT_A_NUMBER;
+		
+	}
+
+	Number::Number( precision_t a_Precision )
+	{
+		m_Exp = 0;
+		setPrecision( a_Precision );
+		m_Sign = NOT_A_NUMBER;
+	}
+
+	Number::Number( const std::string& a_Value, precision_t a_Precision )
+	{
+		setPrecision( a_Precision );
+		set( a_Value );
+	}
+
+	Number::Number( const long long a_Value, precision_t a_Precision )
+	{
+		setPrecision( a_Precision );
+		set( a_Value );
+	}
+
+	// Set methods
+	void Number::set( const Number& a_Value )
+	{
+		m_Exp = a_Value.m_Exp;
+		m_Sign = a_Value.m_Sign;
+		m_Mantissa = a_Value.m_Mantissa;
+	}
+
+	void Number::set( const std::string& a_Value )
+	{
+		// Temporary version!!
+		long long llValue = std::stoll( a_Value );
+		m_Exp = 0;
+		m_Sign = llValue >= 0 ? POSITIVE : NEGATIVE;
+		m_Mantissa[0] = static_cast<mantissa_t>(llValue);
+	}
+
+	void Number::set( const long long a_Value )
+	{
+		m_Exp = 0;
+		m_Sign = a_Value >= 0 ? POSITIVE : NEGATIVE;
+		m_Mantissa[0] = static_cast<mantissa_t>( a_Value );
+	}
+
+	bool Number::operator==( const Number& a_Other ) const
+	{
+		return false;
+	}
+
+	bool Number::operator==( const mpfr_t& a_Other ) const
+	{
+		return false;
+	}
+
+	bool Number::operator!=( const Number& a_Other ) const
+	{
+		return false;
+	}
+
+	bool Number::operator>( const Number& a_Other ) const
+	{
+		return false;
+	}
+
+	bool Number::operator<( const Number& a_Other ) const
+	{
+		return false;
+	}
+
+	bool Number::operator>=( const Number& a_Other ) const
+	{
+		return false;
+	}
+
+	bool Number::operator<=( const Number& a_Other ) const
+	{
+		return false;
+	}
+
+	Number& Number::operator=( Number& a_Other )
+	{
+		set( a_Other );
+		return *this;
+	}
+
+// Private
+	void Number::setPrecision( precision_t a_Precision )
+	{
+		// Precision is given in bits
+		// Will always set precision to the nearest 
+		m_Precision = a_Precision;
+		std::size_t mantissaSizeBits = sizeof( mantissa_t ) * 8;
+
+
+		m_Mantissa.resize( a_Precision / mantissaSizeBits + 1 );
+	}
+
+	// Non member methods
+	void add( Number& a_Result, const Number& a_Num1, const Number& a_Num2, round_t a_Round )
+	{
+		// NOT FINISHED
+		// This line should be removed for efficiency later
+		assert( a_Result.beenInitialised() && a_Num1.beenInitialised() && a_Num2.beenInitialised() );
+
+		a_Result.set( a_Num1.m_Mantissa[0] + a_Num2.m_Mantissa[0] );
+	}
+
+	void sub( Number& a_Result, const Number& a_Num1, const Number& a_Num2, round_t a_Round )
+	{}
+
+	void mult( Number & a_Result, const Number & a_Num1, const Number & a_Num2, round_t a_Round )
+	{}
+
+	void div( Number & a_Result, const Number & a_Num1, const Number & a_Num2, round_t a_Round )
+	{}
+
+}
